afc5362066ce8cd3687b6ead485245d84760a3af
Meisocafe <git.contact@meiso.cafe>
2023-11-29 19:22:36 +0100

    Fix: Missing filters.lua file in 0.2-0 rockspec

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+if ! which ldoc 2> /dev/null; then
+    echo "Please install ldoc to continue"
+    exit 1
+fi
+
 ldoc .
-luarocks make meisocafe.hammerspoon.dkb-0.1-0.rockspec
+luarocks make meisocafe.hammerspoon.dkb-0.2-1.rockspec
